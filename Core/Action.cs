ea4c9db2901e9d5da001e12211ac98e25045b0ae
Kashif-S <35468443+Kashif-S@users.noreply.github.com>
2019-12-08 16:11:46 -0500

    Added interfaces and classes

---
+++ Core/Action.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public interface IAction
+{
+    int GetCost();
+    bool ValidateState(State state);
+    void UpdateState(State state);
+}
