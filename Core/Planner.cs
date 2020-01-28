470fc8d94ae80321e80b244d089591d1aa86e263
Kashif-S <35468443+Kashif-S@users.noreply.github.com>
2020-01-28 16:57:00 -0500

    Updated planner to use heap

--- Core/Planner.cs
+++ Core/Planner.cs
@@ -9,23 +9,17 @@ public class Planner<T, S, A> where T : GoapAgent where S : State where A : Acti
     {
         // TODO: Sort actions based on cost
 
-        // TODO: Replace this with a heap
-        List<PlannerState> openSet = new List<PlannerState>();
+        PlannerStateHeap openSet = new PlannerStateHeap();
         HashSet<S> closedSet = new HashSet<S>();
         openSet.Add(new PlannerState(state, null, null, 0, goal.CalculateHCost(state)));
 
         while (openSet.Count > 0)
         {
-            PlannerState currentState = openSet[0];
-            for (int i = 1; i < openSet.Count; i++)
+            PlannerState currentState = openSet.Pop();
+            if(closedSet.Contains(currentState.state))
             {
-                if (openSet[i].fCost < currentState.fCost || (openSet[i].fCost == currentState.fCost && openSet[i].hCost < currentState.hCost))
-                {
-                    currentState = openSet[i];
-                }
+                continue;
             }
-
-            openSet.Remove(currentState);
             closedSet.Add(currentState.state);
 
             if (goal.ValidateState(currentState.state))
@@ -41,11 +35,7 @@ public class Planner<T, S, A> where T : GoapAgent where S : State where A : Acti
                     int newGCost = currentState.gCost + actions[i].GetCost();
                     int hCost = goal.CalculateHCost(newState);
                     PlannerState newPlannerState = new PlannerState(newState, currentState, actions[i], newGCost, hCost);
-                    if (!openSet.Contains(newPlannerState))
-                    {
-                        Debug.Log(newState.GetHashCode());
-                        openSet.Add(newPlannerState);
-                    }
+                    openSet.Add(newPlannerState);
                 }
             }
         }
@@ -101,6 +91,126 @@ public class Planner<T, S, A> where T : GoapAgent where S : State where A : Acti
         public override int GetHashCode()
         {
             return state.GetHashCode();
+        }
+    }
+
+    private class PlannerStateHeap
+    {
+        private List<PlannerState> Heap = new List<PlannerState>();
+
+        public int Count
+        {
+            get
+            {
+                return Heap.Count;
+            }
+        }
+
+        private static int GetLeftChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 1;
+        }
+        private static int GetRightChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 2;
+        }
+        private static int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        private bool HasLeftChild(int index)
+        {
+            return GetLeftChildIndex(index) < Heap.Count;
+        }
+        private bool HasRightChild(int index)
+        {
+            return GetRightChildIndex(index) < Heap.Count;
+        }
+        private bool HasParent(int index)
+        {
+            return GetParentIndex(index) >= 0;
+        }
+
+        private PlannerState LeftChild(int index)
+        {
+            return Heap[GetLeftChildIndex(index)];
+        }
+        private PlannerState RightChild(int index)
+        {
+            return Heap[GetRightChildIndex(index)];
+        }
+        private PlannerState Parent(int index)
+        {
+            return Heap[GetParentIndex(index)];
+        }
+
+        private void Swap(int indexOne, int indexTwo)
+        {
+            PlannerState temp = Heap[indexOne];
+            Heap[indexOne] = Heap[indexTwo];
+            Heap[indexTwo] = temp;
+        }
+
+        public PlannerState Pop()
+        {
+            if (Heap.Count == 0)
+            {
+                return null;
+            }
+
+            PlannerState state = Heap[0];
+            Heap[0] = Heap[Heap.Count - 1];
+            Heap.RemoveAt(Heap.Count - 1);
+
+            HeapifyDown();
+            return state;
+        }
+
+        public void Add(PlannerState state)
+        {
+            Heap.Add(state);
+            HeapifyUp();
+        }
+
+        private void HeapifyDown()
+        {
+            int index = 0;
+            while(HasLeftChild(index))
+            {
+                int smallerChildIndex = GetLeftChildIndex(index);
+                if(HasRightChild(index) && CompareGreaterThan(LeftChild(index), RightChild(index)))
+                {
+                    smallerChildIndex = GetRightChildIndex(index);
+                }
+
+                if(CompareGreaterThan(Heap[smallerChildIndex], Heap[index])) {
+                    break;
+                } else {
+                    Swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+
+            }
+        }
+
+        private void HeapifyUp()
+        {
+            int index = Heap.Count - 1;
+            while (HasParent(index) && CompareGreaterThan(Parent(index), Heap[index]))
+            {
+                Swap(GetParentIndex(index), index);
+                index = GetParentIndex(index);
+            }
+        }
+
+        private bool CompareGreaterThan(PlannerState left, PlannerState right)
+        {
+            if(left.fCost == right.fCost)
+            {
+                return left.hCost > right.hCost;
+            }
+            return left.fCost > right.fCost;
         }
     }
 }
